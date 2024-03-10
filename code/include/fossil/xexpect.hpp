728525a60a3b08ade79158af796a144610b35b7b
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-03-10 15:47:58 -0600

    Update xexpect.hpp

--- code/include/fossil/xexpect.hpp
+++ code/include/fossil/xexpect.hpp
@@ -314,6 +314,7 @@ TEST_EXPECT(found, "Memory not contains assertion failed");
 // ----------------------------------------------------------------
 // String assertions
 // ----------------------------------------------------------------
+#ifdef __cplusplus
 #define TEST_EXPECT_EQUAL_STRING(actual, expected) TEST_EXPECT((actual) == (expected), "String equality expectation not met")
 #define TEST_EXPECT_NOT_EQUAL_STRING(actual, expected) TEST_EXPECT((actual) != (expected), "String inequality expectation not met")
 #define TEST_EXPECT_LENGTH_STRING(actual, expected) TEST_EXPECT((actual).length() == (expected), "String length expectation not met")
@@ -333,6 +334,27 @@ TEST_EXPECT(found, "Memory not contains assertion failed");
 #define TEST_EXPECT_ENDS_WITH_CSTRING(string, suffix) TEST_EXPECT((strstr(string, suffix) != NULL && strlen(string) >= strlen(suffix) && strcmp(string + strlen(string) - strlen(suffix), suffix)), "String doesn't end with the suffix")
 #define TEST_EXPECT_EQUAL_CASE_INSENSITIVE_CSTRING(actual, expected) TEST_EXPECT(strcmp((actual), (expected)) == 0, "Case-insensitive string equality expectation not met")
 #define TEST_EXPECT_NOT_EQUAL_CASE_INSENSITIVE_CSTRING(actual, expected) TEST_EXPECT(strcmp((actual), (expected)) != 0, "Case-insensitive string inequality expectation not met")
+#else
+#define TEST_EXPECT_EQUAL_CSTRING(actual, expected) TEST_EXPECT(strcmp((actual), (expected)) == 0, "String equality expectation not met")
+#define TEST_EXPECT_NOT_EQUAL_CSTRING(actual, expected) TEST_EXPECT(strcmp((actual), (expected)) != 0, "String inequality expectation not met")
+#define TEST_EXPECT_LENGTH_CSTRING(actual, expected) TEST_EXPECT(strlen((actual)) == (expected), "String length expectation not met")
+#define TEST_EXPECT_CONTAINS_SUBSTRING_CSTRING(string, substring) TEST_EXPECT(strstr((string), (substring)) != NULL, "Substring not found")
+#define TEST_EXPECT_NOT_CONTAINS_SUBSTRING_CSTRING(string, substring) TEST_EXPECT(strstr((string), (substring)) == NULL, "Substring found")
+#define TEST_EXPECT_STARTS_WITH_CSTRING(string, prefix) TEST_EXPECT(strncmp((string), (prefix), strlen(prefix)) == 0, "String doesn't start with the prefix")
+#define TEST_EXPECT_ENDS_WITH_CSTRING(string, suffix) TEST_EXPECT((strstr(string, suffix) != NULL && strlen(string) >= strlen(suffix) && strcmp(string + strlen(string) - strlen(suffix), suffix)), "String doesn't end with the suffix")
+#define TEST_EXPECT_EQUAL_CASE_INSENSITIVE_CSTRING(actual, expected) TEST_EXPECT(strcmp((actual), (expected)) == 0, "Case-insensitive string equality expectation not met")
+#define TEST_EXPECT_NOT_EQUAL_CASE_INSENSITIVE_CSTRING(actual, expected) TEST_EXPECT(strcmp((actual), (expected)) != 0, "Case-insensitive string inequality expectation not met")
+
+#define TEST_EXPECT_EQUAL_STRING(actual, expected) TEST_EXPECT(strcmp((actual), (expected)) == 0, "String equality expectation not met")
+#define TEST_EXPECT_NOT_EQUAL_STRING(actual, expected) TEST_EXPECT(strcmp((actual), (expected)) != 0, "String inequality expectation not met")
+#define TEST_EXPECT_LENGTH_STRING(actual, expected) TEST_EXPECT(strlen((actual)) == (expected), "String length expectation not met")
+#define TEST_EXPECT_CONTAINS_SUBSTRING_STRING(string, substring) TEST_EXPECT(strstr((string), (substring)) != NULL, "Substring not found")
+#define TEST_EXPECT_NOT_CONTAINS_SUBSTRING_STRING(string, substring) TEST_EXPECT(strstr((string), (substring)) == NULL, "Substring found")
+#define TEST_EXPECT_STARTS_WITH_STRING(string, prefix) TEST_EXPECT(strncmp((string), (prefix), strlen(prefix)) == 0, "String doesn't start with the prefix")
+#define TEST_EXPECT_ENDS_WITH_STRING(string, suffix) TEST_EXPECT((strstr(string, suffix) != NULL && strlen(string) >= strlen(suffix) && strcmp(string + strlen(string) - strlen(suffix), suffix)), "String doesn't end with the suffix")
+#define TEST_EXPECT_EQUAL_CASE_INSENSITIVE_STRING(actual, expected) TEST_EXPECT(strcmp((actual), (expected)) == 0, "Case-insensitive string equality expectation not met")
+#define TEST_EXPECT_NOT_EQUAL_CASE_INSENSITIVE_STRING(actual, expected) TEST_EXPECT(strcmp((actual), (expected)) != 0, "Case-insensitive string inequality expectation not met")
+#endif
 
 // ----------------------------------------------------------------
 // Char assertions
